ceef8d026a8f713a01d8d764188c6e107b42c8fd
IDSCAN\bmulhall <bmulhall@idscan.net>
2021-01-14 15:23:03 -0600

    Add project files.

---
+++ dot-net-facial-recognition-sdk-sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot_net_facial_recognition_sdk_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
